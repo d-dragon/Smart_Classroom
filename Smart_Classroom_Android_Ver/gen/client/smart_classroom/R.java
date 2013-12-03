11b3f5ec25784c983b679b6ae566dc3f74280452
d-dragon <phanduyt291@gmail.com>
2013-12-03 10:42:28 +0700

    TCP Socket OK!

--- Smart_Classroom_Android_Ver/gen/client/smart_classroom/R.java
+++ Smart_Classroom_Android_Ver/gen/client/smart_classroom/R.java
@@ -28,8 +28,9 @@ public final class R {
         public static final int bConnect=0x7f050002;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int logo=0x7f020001;
+        public static final int e_icon=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int logo=0x7f020002;
     }
     public static final class id {
         public static final int action_settings=0x7f090012;
