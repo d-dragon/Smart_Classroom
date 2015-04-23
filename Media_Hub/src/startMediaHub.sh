a17ae45d07e0d8096217a7c53345da1614d044ae
d-dragon <phanduyt291@gmail.com>
2015-04-23 14:49:17 +0700

    update start app script

--- Media_Hub/src/startMediaHub.sh
+++ Media_Hub/src/startMediaHub.sh
@@ -1,7 +1,44 @@
-#!/bin/sh 
+#!/bin/bash 
+#get network interface ip
+flag=0
+echo $1
+while true; do 
+ip=`ifconfig | grep -A 1 $1 | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1`
+mask=`ifconfig | grep -A 1 $1 | tail -1 | cut -d ':' -f 4`
+gw=`route -n | grep 'UG[ \t]' | awk '{print $2}'`
+echo "ip=$ip"
+echo "netmask=$mask"
+echo "gateway=$gw"
+#mask="255.255.255.0"
+#res="${mask//[^255]}"
 
-while true; do
-        ./home/pi/Smart_Classroom/Media_Hub/src/audio_app &
-        killall /home/pi/Smart_Classroom/Media_Hub/src/audio_app
+#subnet_oct=`grep -o '255' <<<"$res" | grep -c .`
+#let subnet_rev=5-subnet_oct
+#echo $subnet_oct
+
+#gw=`echo $ip | rev | cut -f$subnet_rev- -d "." | rev`
+#case "$subnet_oct" in
+#1)	gw+=".1.1.1"
+#	;;
+#2)	gw+=".1.1"
+#	;;
+#3)	gw+=".1"
+#esac
+#echo $gw
+# -q quiet
+# -c nb of pings to perform
+ping -q -c5 $gw > /dev/null
+
+if [ $? -eq 0 ]
+then
+	echo "network is ok"
+	flag=1
+fi
+if [ $flag -eq 1 ]
+then
+#        ./home/pi/Smart_Classroom/Media_Hub/src/audio_app &
+#        killall /home/pi/Smart_Classroom/Media_Hub/src/audio_app
+	flag=0
+fi
 done
 
